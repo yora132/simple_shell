468b5540d9cf1d86b9ec28700a003c52e03ed5a1
yora132 <yarasalah132@gmail.com>
2023-04-23 06:48:25 -0700

    first

--- hsh/main.c
+++ hsh/main.c
@@ -3,5 +3,22 @@
 
 int main(int ac, char **argv)
 {
+	char *prompt = "(hsh) $";
+	char *lineptr;
+	size_t n = 0;
+	(void)ac; (void)argv;
+
+	while (1)
+	{
+	printf("%s", prompt);
+	if (getline(&lineptr, &n, stdin) == -1)
+	{
+		return (-1);
+	}
+	printf("%s\n", lineptr);
+	free(lineptr);
+	lineptr = NULL;
+	n = 0;
+	}
 	return (0);
 }
--- hsh/shell.h
+++ hsh/shell.h
@@ -4,3 +4,5 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+
+#endif
